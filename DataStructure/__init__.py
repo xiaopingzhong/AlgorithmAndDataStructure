cedb4020ddc3290072a24a59a0b4742beee63903
xiaopingzhong <zhongxiaoping@tju.edu.cn>
2019-07-12 14:07:36 +0800

    完善文档

---
+++ DataStructure/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+************************************************
+@Time    : 2019-07-11 12:53
+@Author  : zxp
+@Project : AlgorithmAndDataStructure
+@File    : __init__.py.py
+@Description: ==================================
+    
+@license: (C) Copyright 2013-2019.    
+************************************************
+"""
