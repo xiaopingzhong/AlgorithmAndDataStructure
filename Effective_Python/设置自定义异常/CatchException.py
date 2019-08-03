f1d9c6b5e153b2061c0468d291b64955a09c7cad
xiaopingzhong <zhongxiaoping@tju.edu.cn>
2019-08-03 22:08:55 +0800

    增加对自定义异常的注释

--- Effective_Python/设置自定义异常/CatchException.py
+++ Effective_Python/设置自定义异常/CatchException.py
@@ -18,7 +18,12 @@ def catchException():
     try:
         # 很关键,指定所抛出的异常类型名称
         testRaise()
+    # 因此except直接捕捉的是自定义类型,
+    # 而不需要哪种
+    # except Exception as e:
+    # 此时,因此那个此时e是ExceptionInfo类型
     except PreconditionsException as e:
+        print(type(e))  # PreconditionsErr类型
         # 打印异常的名称
         print(e)
 
