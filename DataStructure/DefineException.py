227626ed2975e4689fe4a62c9027e5a3c29bcc39
xiaopingzhong <zhongxiaoping@tju.edu.cn>
2019-08-03 22:13:22 +0800

    设置专门的自定义异常

---
+++ DataStructure/DefineException.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+************************************************
+@Time    : 2019-08-03 18:36
+@Author  : zxp
+@Project : AlgorithmAndDataStructure
+@File    : DefineException.py
+@Description: ==================================
+    
+@license: (C) Copyright 2013-2019.    
+************************************************
+"""
+
+
+class FullException(Exception):
+    """已经满了的异常"""
+
+    def __init__(self, err=None):
+        Exception.__init__(self, err)
+
+    @property
+    def fullExceptiontoString(self):
+        return str(self)
+
+
+class EmptyException(Exception):
+    """已经满了的异常"""
+
+    def __init__(self, err=None):
+        Exception.__init__(self, err)
+
+    @property
+    def emptyExceptiontoString(self):
+        return str(self)
